f3f93dfadb6499ef7839a02bd4f3eaf55ec020ae
Vinod Halaharvi <vinod.halaharvi@gmail.com>
2013-11-13 11:52:07 -0800

    Initial Commit

--- dbscan_wiki.py
+++ dbscan_wiki.py
@@ -1,6 +1,28 @@
 import sys
+import math
 import csv
 import numpy as np
+
+class Point(object):
+	"""docstring for Point"""
+	def __init__(self, _list):
+		super(Point, self).__init__()
+		if not _list:
+			self.list = []
+		else:
+			self.list = _list
+
+		self.visited = False
+		self.isClusterMember = False
+
+	def tolist(self):
+		"""docstring for tolist"""
+		return self.list
+	def __sub__(self, other):
+		"""docstring for __sub__"""
+		assert len(self.list) == len(other)
+		return [self.list[i]-other[i] for i, _ in enumerate(self.list)]
+		
 
 class DBSCAN(object):
 	"""docstring for DBSCAN"""
@@ -9,14 +31,15 @@ class DBSCAN(object):
 
 	def dbscan(self, D, eps, MinPts):
 		C = 0
-		for P in D and not P.visited:
-			P.visited = True
-			NeighborPts = self.regionQuery(P, eps)
-			if sizeof(NeighborPts) < MinPts:
-				P.NOISE = True
-			else:
-				C = []
-				self.expandCluster(P, NeighborPts, C, eps, MinPts)
+		for P in D:
+			if not P.visited:
+				P.visited = True
+				NeighborPts = self.regionQuery(P, eps)
+				if sizeof(NeighborPts) < MinPts:
+					P.NOISE = True
+				else:
+					C = []
+					self.expandCluster(P, NeighborPts, C, eps, MinPts)
 	       
 	def expandCluster(self, P, NeighborPts, C, eps, MinPts):
 		C.append(P)
@@ -37,14 +60,14 @@ class DBSCAN(object):
 		  
 	def regionQuery(self, P, eps):
 		"""docstring for passre"""
-		return [ point for point in D if in_range(point, P)]
+		return [ point for point in D if self.in_range(point, P)]
 
 			
 if __name__ == '__main__':
 	assert len(sys.argv) > 1, " Input file must be provided."
 	input_file_name = sys.argv[1]
-	D = np.float32(np.genfromtxt(input_file_name, delimiter=','))
-	print D
+	D = np.float32(np.genfromtxt(input_file_name, delimiter=',')).tolist()
+	D = [Point(p) for p in D]
 	eps = 0.0003
 	MinPts = 10
 	dbs = DBSCAN().dbscan(D, eps, MinPts)
