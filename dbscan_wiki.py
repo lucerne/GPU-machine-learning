81eefa5d701a8c9bff4542780cfcd94e823ef159
Vinod Halaharvi <vinod.halaharvi@gmail.com>
2013-11-13 16:46:41 -0800

    dbscan serial close to working

--- dbscan_wiki.py
+++ dbscan_wiki.py
@@ -3,6 +3,7 @@ import math
 import csv
 import numpy as np
 
+MAX_NUM_OF_CLUSTERS = 10 
 class Point(np.ndarray):
 
     def __new__(subtype, shape, dtype=float, buffer=None, offset=0,
@@ -24,7 +25,9 @@ class DBSCAN(object):
 		super(DBSCAN, self).__init__()
 
 	def dbscan(self, D, eps, MinPts):
-		C = 0
+		count = 0 
+		C = [[] for _ in range(MAX_NUM_OF_CLUSTERS)]
+		print C
 		for P in D:
 			if not P.visited:
 				P.visited = True
@@ -32,11 +35,14 @@ class DBSCAN(object):
 				if len(NeighborPts) < MinPts:
 					P.NOISE = True
 				else:
-					C = []
-					self.expandCluster(P, NeighborPts, C, eps, MinPts)
+					# pick new cluster
+					count += 1
+					c = C[count]
+					self.expandCluster(P, NeighborPts, c, eps, MinPts)
 	       
-	def expandCluster(self, P, NeighborPts, C, eps, MinPts):
-		C.append(P)
+	def expandCluster(self, P, NeighborPts, c, eps, MinPts):
+		c.append(P)
+		NeighborPtsCopy = NeighborPts.copy()
 		for P_dash in NeighborPts:
 			if not P_dash.visited:
 				P_dash.visited = True
@@ -44,7 +50,7 @@ class DBSCAN(object):
 				if len(NeighborPts_dash) >= MinPts:
 					NeighborPts = NeighborPts.extend(NeighborPts_dash)
 				if not P_dash.isClusterMember:
-					C.append(P_dash)
+					c.append(P_dash)
 					P_dash.isClusterMember = True
 
 	def in_range(self, point, P_dash):
@@ -54,14 +60,25 @@ class DBSCAN(object):
 		  
 	def regionQuery(self, P, eps):
 		"""docstring for regionQuery"""
-		return [point for point in D if self.in_range(point, P)]
+		#return [point for point in D if self.in_range(point, P)]
+		result =  [point for point in D if self.in_range(point, P)]
+		if result is None:
+			result = []
+		print "***********************"
+		print P
+		print result
+		print
+		print "***********************"
+		return result
 
 			
 if __name__ == '__main__':
 	assert len(sys.argv) > 1, " Input file must be provided."
 	input_file_name = sys.argv[1]
-	D = np.genfromtxt(input_file_name, delimiter=',')
-	D = Point(D.shape, dtype=np.float32)
-	eps = 0.0003
-	MinPts = 10
+	X = np.genfromtxt(input_file_name, delimiter=',')
+	D = Point(X.shape, dtype=np.float32)
+	D[:] = X[:]
+	eps = 0.3
+	MinPts =1 
 	dbs = DBSCAN().dbscan(D, eps, MinPts)
+
